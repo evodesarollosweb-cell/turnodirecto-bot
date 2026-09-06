85ebe38cdaeaf16da92b083f315f6f0f13712abc
evodesarollosweb-cell <evodesarollosweb@gmail.com>
2026-09-06 19:16:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,17 @@ const openrouter = new OpenAI({
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
   }
 });
 
